cc96f629f8284c96d459db30b85ae167e7fcc965
pavelpugacenko <pugachenkopavel72@gmail.com>
2024-12-10 19:51:37 +0500

    commit 2

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+
+def home_page(request):
+    return render(request, 'catalog/home.html')
+
+
+def contacts_page(request):
+    return render(request, 'catalog/contacts.html')
+
 # Create your views here.
